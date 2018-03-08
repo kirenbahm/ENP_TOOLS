0504444314d4c85071edea620cf757c04ecb3a90
unknown <georgio@gitconsulting.net>
2018-03-08 11:44:51 -0500

    Modified caption of ficures

--- ENPMS/LIB/util/generate_page_figures.m
+++ ENPMS/LIB/util/generate_page_figures.m
@@ -118,7 +118,7 @@ end
 function tail_figure(fidTEX,RFIGURE,RLABEL,FIGURE)
 
 % add caption
-ROW5 =[' \caption[Station ' char(RFIGURE) ']{Station: ' char(RFIGURE) ', AREA: ' char(RLABEL) '}'];
+ROW5 =[' \caption[Station ' char(RFIGURE) ']{Station: ' char(RFIGURE)];
 %   ROW5 =[' \caption[ ' char(RFIGURE) ']{' char(RFIGURE) '}'];
 fprintf(fidTEX,'%s\n',ROW5);
 

26249f01260681bc00d7c00f1900e44680a14d6a
kiren <kiren_bahm@nps.gov>
2018-09-26 11:14:28 -0400

    Preproc mods - Changed D01 INI variable names
    Changed names of variables in the INI master variable, reorganized
    scripts, and modified print statements.

--- PRE_PROCESSING/D01_convert_DFE_to_DFS0.m
+++ PRE_PROCESSING/D01_convert_DFE_to_DFS0.m
@@ -1,42 +1,44 @@
 function D01_convert_DFE_to_DFS0()
-% Script reads station data and generates a set of dfs0 files which use the
-% original timeseries intervals. The script requires a directory which
-% which is named DFS0, in this directory the *.out files reside. A dfs0
-% file is created for each .out file. The .dfs0 file uses the original
-% timestep. 
+% This script converts data files from the DataForEver
+% database (DFE format) output format to the DHI MIKE data format (DFS format)
+% The script expects files to be timeseries data with only one
+% station-datatype pair in each file. The files are additionally sorted in
+% to unique directories based on datatype.
 
 % -------------------------------------------------------------------------
-% path string of ROOT Directory = DRIVE:/GIT/ENP_TOOLS MAIN Directory = PRE_PROCESSING
 % -------------------------------------------------------------------------
-[ROOT,MAIN,~] = fileparts(pwd());
-TEMP = strsplit(ROOT,'\');
+% BEGIN USER INPUT
+% -------------------------------------------------------------------------
+% -------------------------------------------------------------------------
 
-INI.ROOT = [TEMP{1} '/' TEMP{2} '/'];
+% Location of raw DFE data files (each datatype needs to be in a separate folder)
+INI.OBS_FLOW_DFE_DIR  = '../../ENP_TOOLS_Sample_Input/Raw_DFE_Data/Flow/';
+INI.OBS_STAGE_DFE_DIR = '../../ENP_TOOLS_Sample_Input/Raw_DFE_Data/Stage/';
 
-% -------------------------------------------------------------------------
-% Add path(s) to ENP_TOOLS and all other 1st level sub-directories
-% -------------------------------------------------------------------------
-INI.TOOLS_DIR = [INI.ROOT TEMP{3} '/'];
-INI.SAMPLE_INPUT_DIR = [INI.ROOT 'ENP_TOOLS_Sample_Input/'];
+% Suffix of raw DFE data files (used to generate a list of files to process)
+INI.OBS_DFE_FILETYPE = '*.dat';
 
-clear TEMP ROOT MAIN
-% -------------------------------------------------------------------------
-% Add sub--directory path(s) for ENP_TOOLS directory
-% -------------------------------------------------------------------------
-INI.PRE_PROCESSING_DIR = [INI.TOOLS_DIR MAIN '/'];
-    % Input directories:
-INI.input = [INI.PRE_PROCESSING_DIR '_input/'];
-    % DFS0 file creation from DFE input file directories
-INI.STATION_DIR = [INI.PRE_PROCESSING_DIR 'D00_STATIONS/'];
-INI.FLOW_DIR = [INI.PRE_PROCESSING_DIR 'D01_FLOW/'];
-INI.STAGE_DIR = [INI.PRE_PROCESSING_DIR 'D02_STAGE/'];
-    % BC2D generation directories:
-INI.BC2D_DIR = [INI.PRE_PROCESSING_DIR 'G01_BC2D/'];
+% Location of dfs0 output files (each datatype needs a separate folder)
+INI.DIR_FLOW_DFS0     = '../../ENP_TOOLS_Sample_Input/Obs_Data_Processed/D01_FLOW/DFS0/';
+INI.DIR_STAGE_DFS0    = '../../ENP_TOOLS_Sample_Input/Obs_Data_Processed/D02_STAGE/DFS0/';
 
-% -------------------------------------------------------------------------
-% SETUP Location of ENPMS Scripts and Initialize
-% -------------------------------------------------------------------------
+% Location of ENPMS library
 INI.MATLAB_SCRIPTS = '../ENPMS/';
+
+% Other options (0 = NO, 1 = YES)
+INI.DEBUG = 0;                 % Print extra debugging output?
+INI.DELETE_EXISTING_DFS0 = 1;  % Delete existing DFS0 files? Has this option been checked?
+
+
+% -------------------------------------------------------------------------
+% -------------------------------------------------------------------------
+% END USER INPUT
+% -------------------------------------------------------------------------
+% -------------------------------------------------------------------------
+
+% currently inactive options (not tested):
+%INI.SAVE_IN_MATLAB = 1;  % Save in MATLAB format? 
+
 
 try
     addpath(genpath(INI.MATLAB_SCRIPTS));
@@ -44,29 +46,28 @@ catch
     addpath(genpath(INI.MATLAB_SCRIPTS,0));
 end
 
-% Save in MATLAB format? (0 = FALSE, 1 = TRUE)
-SAVE_IN_MATLAB = 1;
-
-% Delete existing DFS0 files? (0 = FALSE, 1 = TRUE)
-INI.DELETE_EXISTING_DFS0 = 1;
-
 % iterate over input file datatype directories with DFE *.dat files:
 for DType_Flag = {'Water Level','Discharge'}
 %for DType_Flag = {'Discharge','Water Elevation'}
     if strcmpi(DType_Flag,'Discharge')
-        INI.DIR_DFS0_FILES = [INI.FLOW_DIR 'DFS0/'];                       % set output location fro DFS0 files to proper directory
-        FILE_FILTER = [INI.input 'Flow/*.dat'];                            % list only files with extension *.dat
-        DIR_FILES = FILE_FILTER;
-        LISTING  = dir(char(DIR_FILES));
+        FILE_FILTER = [INI.OBS_FLOW_DFE_DIR INI.OBS_DFE_FILETYPE];                            % list only files with extension *.dat
+        LISTING  = dir(char(FILE_FILTER));
+        INI.DIR_DFS0_FILES = INI.DIR_FLOW_DFS0;
+        fprintf('\n');
+        fprintf('Input files being read from: %s\n', INI.OBS_FLOW_DFE_DIR);
+        fprintf('Output files written to: %s\n',     INI.DIR_DFS0_FILES);
     elseif strcmpi(DType_Flag,'Water Level')
-        INI.DIR_DFS0_FILES = [INI.STAGE_DIR 'DFS0/'];                      % set output location fro DFS0 files to proper directory
-        FILE_FILTER = [INI.input 'Stage/*.dat'];                           % list only files with extension *.dat
-        DIR_FILES = FILE_FILTER;
-        LISTING  = dir(char(DIR_FILES));
+        FILE_FILTER = [INI.OBS_STAGE_DFE_DIR INI.OBS_DFE_FILETYPE];                           % list only files with extension *.dat
+        LISTING  = dir(char(FILE_FILTER));
+        INI.DIR_DFS0_FILES = INI.DIR_STAGE_DFS0;
+        fprintf('\n');
+        fprintf('Input files being read from: %s\n', INI.OBS_STAGE_DFE_DIR);
+        fprintf('Output files written to: %s\n',     INI.DIR_DFS0_FILES);
     end
     D02_process_file_list(INI,LISTING,DType_Flag);                   % submit output DFS0 directory, input file list, and data type flag for processing
 end
 
 fclose('all');
+fprintf('\n DONE \n\n');
 
 end

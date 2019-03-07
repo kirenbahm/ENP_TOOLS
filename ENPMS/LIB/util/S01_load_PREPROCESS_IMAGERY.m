8e7506d1fa92bea49c122b955a0358133790a2fd
kiren <kiren_bahm@nps.gov>
2019-03-07 12:19:50 -0500

    Preproc - simplified and commented kml scripts
    Added and modified comments in these 2 scripts.
    Moved S01 subfunctions into util directory.
    Simplified arguments sent to S01 function.

--- ENPMS/LIB/util/S01_load_PREPROCESS_IMAGERY.m
+++ ENPMS/LIB/util/S01_load_PREPROCESS_IMAGERY.m
@@ -1,16 +1,16 @@
-function [IMAGE_FILES,KEYS] = S01_load_PREPROCESS_IMAGERY(INI,DType_Flag,DFS0_TYPE,FILE_FILTER,MAP_STATIONS)
+function [IMAGE_FILES,KEYS] = S01_load_PREPROCESS_IMAGERY(DType_Flag,FILE_FILTER,MAP_STATIONS)
 %Function uses *.PNG image files of observed DFE data used in DFS0 file
 %  creation. The script searches all KNOWN and LISTED datatype (DType_Flag) 
 %  directories separating the images by CHART TYPE building a *.kml file
 %  for usage with GOOGLE EARTH.
+%
+% Expected filename format is: station.dataype-plottype.png
 
-% Location of ENPMS library
-INI.MATLAB_SCRIPTS = '../ENPMS/';
 IMAGE_FILES = containers.Map();
 LISTING  = dir(char(FILE_FILTER));
 NLISTING = length(LISTING);
 KEYS = cell(1,NLISTING);
-FILE = struct('layer',cell(1,NLISTING),'folder',cell(1,NLISTING),'name',...
+FILE_INFO = struct('layer',cell(1,NLISTING),'folder',cell(1,NLISTING),'name',...
     cell(1,NLISTING),'station',cell(1,NLISTING),'datatype',cell(1,NLISTING),...
     'chart',cell(1,NLISTING),'lat',cell(1,NLISTING),'long',cell(1,NLISTING));
 for jj = 1:length(LISTING)
@@ -18,19 +18,18 @@ for jj = 1:length(LISTING)
         temp = strsplit(LISTING(jj).name,'.');
         fileNAME_parts = [temp{1} strsplit(temp{2},'-') temp{3}];
         KEYS{jj} = LISTING(jj).name;
-        FILE(jj).layer = char(DType_Flag);
-        FILE(jj).folder = fileNAME_parts{3};
-        FILE(jj).name = LISTING(jj).name;
-        FILE(jj).station = fileNAME_parts{1};
-        FILE(jj).datatype = fileNAME_parts{2};
-        FILE(jj).chart = fileNAME_parts{3};
-        FILE(jj).lat = MAP_STATIONS(fileNAME_parts{1}).LAT;
-        FILE(jj).long = MAP_STATIONS(fileNAME_parts{1}).LONG;
-        IMAGE_FILES(char(KEYS(jj))) = FILE(jj);
+        FILE_INFO(jj).layer = char(DType_Flag);
+        FILE_INFO(jj).folder = fileNAME_parts{3};
+        FILE_INFO(jj).name = LISTING(jj).name;
+        FILE_INFO(jj).station = fileNAME_parts{1};
+        FILE_INFO(jj).datatype = fileNAME_parts{2};
+        FILE_INFO(jj).chart = fileNAME_parts{3};
+        FILE_INFO(jj).lat = MAP_STATIONS(fileNAME_parts{1}).LAT;
+        FILE_INFO(jj).long = MAP_STATIONS(fileNAME_parts{1}).LONG;
+        IMAGE_FILES(char(KEYS(jj))) = FILE_INFO(jj);
     catch
         fprintf('\n ERROR LOADING: %s\n', LISTING(jj).name)
     end
 end
 
-fprintf('\n Images LOADED: %s - %s \n\n', char(DType_Flag), char(DFS0_TYPE))
 end

450ef9c623bf3ebf31e48e5a43eb33cc24f551da
kiren <kiren_bahm@nps.gov>
2020-04-17 16:06:54 -0400

    preproc updates - variable renaming
    updated names of variables to more descriptive names
    and added comments

--- ENPMS/LIB/util/preproc_read_DFE_file.m
+++ ENPMS/LIB/util/preproc_read_DFE_file.m
@@ -1,8 +1,10 @@
 function [DATA] = preproc_read_DFE_file(INI, fileID)
 
+% expected data format:  station_name|datatype|date-time(YYYY-MM-DD HH:MM)|measurement_value
+
 formatString = '%s %s %s %s %*[^\n]';
 
-n = 100000; % Read file in blocks in 10,000 lines, 
+n = 100000; % Read file in blocks in 10,000 lines,
 i = uint64(0); % counter
 
 % initialize maps and vectors
@@ -12,7 +14,7 @@ i = uint64(0); % counter
 % Q_VALUE = []; (unused)
 % H_TIME = []; (unused)
 % H_VALUE = []; (unused)
-% 
+%
 % CURRENT_STATION = ''; (unused)
 % n_st = 1; (unused)
 FIELD_STATION = [];
@@ -23,56 +25,81 @@ FIELD_MEASUREMENTS = [];
 %tic;
 while ~feof(fileID)
 
-    D = textscan(fileID,formatString,'HeaderLines',9,'Delimiter','|','EmptyValue',NaN);
+    fileData = textscan(fileID,formatString,'HeaderLines',9,'Delimiter','|','EmptyValue',NaN);
     i = i + n;
-   try
-      % Field 1 is station name. Convert this to uppercase
-        STATION = upper(D{1}); % convert station to upper case
-      % Field 2 is datatype. This is not used.
-        %DTYPE = D{2}; (unused)
-      % Field 3 is date or date-time
-      % Field 4 is the measurement value
-        D{5}=str2double(strrep(D{4},'null',''));                                                                  % Create new cell array column of measurement values
-        B=split(D{3},' '); E = cellstr(B(:,1)); F = cellstr(strrep(B(:,2),':','')); % Split the date-time cell column into separate cell columns
-        D{3} = E; D{4} = F; clear ('B','E','F')                                     % Rejoin cell columns to the cell array 'D' and clear excess variables
-        % this section provides alternative handling of the hour:minute
-        % values
-        DN = str2double(D{4});
-        if isnan(DN(1)), DN(1) = 0; end
-        D0 = sprintf('%04d',DN(1));
-        if length(DN) > 1
-            for i2 = 2:length(DN)
-                dn = DN(i2);
-                if isnan(dn), dn = 0; end
-                D0 = [D0; sprintf('%04d',dn)];
+    try
+        %-----------------------------------
+        % FIELD 1:  STATION NAME
+        %-----------------------------------
+        %   Convert this to uppercase
+        STATION = upper(fileData{1});
+        
+        %-----------------------------------
+        % FIELD 2:  DATATYPE
+        %-----------------------------------
+        %   Datatype is not used
+        % DTYPE = fileData{2};
+        
+
+        %-----------------------------------
+        % FIELD 4:  MEASUREMENT VALUE
+        %-----------------------------------
+        % Take measurement value strings, remove any strings that say 'null', convert to double, and save
+        MEASUREMENTS = str2double(strrep(fileData{4},'null',''));
+        
+        %-----------------------------------
+        % FIELD 3:  DATE ~OR~ DATE-TIME
+        %-----------------------------------
+
+        myDATEandTIME=split(fileData{3},' '); % Split the date-time cell column into separate cell columns
+        myDATE = cellstr(myDATEandTIME(:,1)); % Put date strings into one cell array (YYYY-MM-DD)
+        myTIME = cellstr(strrep(myDATEandTIME(:,2),':','')); % Put time strings into a call array without ':' (HHMM)
+
+        %-----
+        % this section provides alternative handling of the hour:minute values
+        myTIMEdouble = str2double(myTIME);
+
+        % evaluate first element in the array, and write first element in new array
+        if isnan(myTIMEdouble(1)), myTIMEdouble(1) = 0; end
+        myOtherTimesString = sprintf('%04d',myTIMEdouble(1));
+        
+        % if more than one element in array, evaluate and append subsequent elements to new array
+        if length(myTIMEdouble) > 1
+            for myTimeIndex = 2:length(myTIMEdouble)
+                currentIndexTime = myTIMEdouble(myTimeIndex);
+                if isnan(currentIndexTime), currentIndexTime = 0; end
+                myOtherTimesString = [myOtherTimesString; sprintf('%04d',currentIndexTime)];
             end
         end
-        TSTR = strcat(D{3},D0);
-        % end alternative handling
         
-        TIME = datenum(TSTR,'yyyy-mm-ddHHMM');
-        DATEVEC = datevec(TIME);
-        DATESTR = datestr(DATEVEC,31);
-        MEASUREMENTS = D{5};
-
-        % find all NaNs in the data vector
+        TSTR = strcat(myDATE,myOtherTimesString); % concatenate into new array, format 'yyyy-mm-ddHHMM'
+        % end alternative handling
+        %-----
+        
+        myTIME = datenum(TSTR,'yyyy-mm-ddHHMM');
+        myDATEVEC = datevec(myTIME);
+        myDATEandTIMEstring = datestr(myDATEVEC,31);
+        
+        %-----------------------------------
+        % find all NaNs in the data vector, and erase all associated rows in other arrays
         IND =isnan(MEASUREMENTS);
         
-        % Erase all  rows that have V=NaN's
         STATION(IND) = [];
         %DTYPE(IND) = []; (unused)
-        TIME(IND) = [];
+        myTIME(IND) = [];
         MEASUREMENTS(IND) = [];
+        
+        %-----------------------------------
         
         FIELD_STATION = [FIELD_STATION; STATION];
         %FIELD_DTYPE = [FIELD_DTYPE; DTYPE]; (unused)
-        FIELD_TIME = [FIELD_TIME; TIME];
+        FIELD_TIME = [FIELD_TIME; myTIME];
         FIELD_MEASUREMENTS = [FIELD_MEASUREMENTS; MEASUREMENTS];
         
     catch
         if INI.DEBUG
             fprintf('EXCEPTION: %d::%s::%s::D0 = %s\n', i, char(STATION(1)), ...
-                char(TSTR(1)),char(D0(1)));
+                char(TSTR(1)),char(myOtherTimesString(1)));
             fprintf('EXCEPTION::CONTINUING::\n');
             %        error(errorStruct);
         end
@@ -81,13 +108,17 @@ while ~feof(fileID)
     if INI.DEBUG && (mod(i,n)==0 || ~feof(fileID))
         toc;
         fprintf('... %d\t:%s:: %s: %s :: %s\n', length(FIELD_MEASUREMENTS), ...
-            char(STATION(1)), DATESTR(1), MEASUREMENTS(1));
+            char(STATION(1)), myDATEandTIMEstring(1), MEASUREMENTS(1));
         tic;
     end
 end
 
 % eliminate dates earlier than 1960, there were some errors in the database
 % resulting in reading dates that were negative (once case is S_331_S_173)
+
+fprintf('\n ...eliminating dates earlier than 1960, there were some errors in the database ');
+fprintf('resulting in reading dates that were negative (once case is S_331_S_173). ');
+fprintf('SHOULD WE BE DOING THIS?\n');
 
 IND = find(FIELD_TIME<715876);
 FIELD_STATION(IND) = [];

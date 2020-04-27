8d3b61099efb4503d82483bdf3ea410267ccbc29
LAGO Support <62208225+LAGO-Support@users.noreply.github.com>
2020-04-27 13:14:13 -0400

    Update readFileCompCoord.m

--- ENPMS/LIB/util/readFileCompCoord.m
+++ ENPMS/LIB/util/readFileCompCoord.m
@@ -5,6 +5,7 @@ function INI = readFileCompCoord(INI)
 
 % Create empty container
 INI.mapCompSelected = containers.Map;
+dfs2Codes = readDomainGridCodes(INI.DomainDfs2);
 
 % Read Excel spreadsheet into generic data arrays
 [NUM,TXT,RAW] = xlsread(char(INI.fileCompCoord),char(INI.XLSCOMP));
@@ -24,6 +25,23 @@ for i = 2:numRows % each row has data for a different station
         stationComputed.Z = cell2mat(RAW(i,8));
         stationComputed.I = cell2mat(RAW(i,15));
         stationComputed.J = cell2mat(RAW(i,16));
+        if(dfs2Codes.saxis.X0 ~= 0 && dfs2Codes.saxis.Y0 ~= 0)
+          % check of station indexes
+          i1= (stationComputed.X_UTM-dfs2Codes.saxis.X0)/dfs2Codes.saxis.Dx; % Converting to index
+          j1= (stationComputed.Y_UTM-dfs2Codes.saxis.Y0)/dfs2Codes.saxis.Dy; % Converting to index
+          if(abs(i1 - stationComputed.I) > 0.5 || abs(j1 - stationComputed.J) > 0.5)
+            fprintf('--- Warning: Station %s at excel row %i with a coordinate indexes of (%i , %i) is estimated as (%i , %i) based on model domain dfs2\n',...
+            char(stationComputed.STATION_NAME), i, stationComputed.I, stationComputed.J, i1, j1);
+            %stationComputed.I = i1;  
+            %stationComputed.J = j1;
+          end
+        end
+        % Here we are only accepting cells of GridCode value 1, change last condition to == 0 to accept border as well
+        if((stationComputed.I + 1 > dfs2Codes.Cols || stationComputed.I + 1 <= 0) || (stationComputed.J + 1 > dfs2Codes.Rows || stationComputed.J + 1 <= 0)...
+         || dfs2Codes.V(stationComputed.J + 1, stationComputed.I + 1) ~= 1)
+         stationComputed.I = 0;
+         stationComputed.J = 0;
+        end
         stationComputed.M11CHAIN = '';
         stationComputed.N_AREA = char(TXT(i,18));
         stationComputed.I_AREA = cell2mat(RAW(i,19));

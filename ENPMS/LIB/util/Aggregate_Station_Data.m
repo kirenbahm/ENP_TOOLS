a80c7787de5a9876da9facc03b80a6d144867292
LAGO Support <62208225+LAGO-Support@users.noreply.github.com>
2020-05-26 13:10:56 -0400

    MoreDetails
    Added More comments to understand flow of program and made command window statements of progress on equation easier to understand

--- ENPMS/LIB/util/Aggregate_Station_Data.m
+++ ENPMS/LIB/util/Aggregate_Station_Data.m
@@ -1,41 +1,65 @@
-function INI = Aggregate_Stations (INI)
+function INI = Aggregate_Station_Data (INI)
+
+%---------------------------------------------------------------------
+%---------------------------------------------------------------------
+%  README for this function
+%---------------------------------------------------------------------
+%---------------------------------------------------------------------
+%   This function reads a text file containing equation to create aggregate
+%   stations, then creates a new station in the database for the aggrgate
+%   station. It then reads the flow data from the station to sum based on
+%   the equation and stores the new aggrgate timeseries data in the new
+%   station.
+
+% Function flows as follows.
+% Read a line
+% Check if line is comment or empty, if not attempt to parse equation.
+% First check is if new Aggregate Station Name is taken, if not continue
+% If operators are in correct positions the equation is valid and continues
+% If Station is found, data is daily, and flow then staion is used, if not skipped
+% If there is one valid station,
+% Then the new Aggregate station will be calculated and added to database.
+% Else if will not be calculated.
+
 % Read Text file with equations
-StationEqFile = [INI.DATA_COMMON 'aggregate_rules_file_example.txt'];
-fid = fopen(StationEqFile);
+fid = fopen(INI.AGGREGATE_EQUATIONS);
+fprintf('\n--Reading Aggregate Staions Equations from file....\n');
 tline = fgetl(fid);
 stations = {};
+fprintf('--Creating Aggregate Staions based on file Equations....\n');
+% while loop reads file line by line
 while ischar(tline)
-    % Line isn't empty and doesn't contain a comment 
+    % If line isn't empty and doesn't contain a comment then parse equation
     if ~contains(tline,'#') && ~isempty(tline)
-        parts = strsplit(tline, ' '); % split line based on spaces Ex A = B + C => [{A} {=} {B} {+} {C}]
+        fprintf('\n----Parsing Equation\n');
+        fprintf('----%s\n', tline);
+        parts = strsplit(tline, ' '); % split line based on spaces Ex. A = B + C => [{A} {=} {B} {+} {C}]
         sizeparts = size(parts); % get size of split
         pass = true;
         try
             % Block will complete if desired Aggregate station name is
             % already taken
+            EqReport = {}; % Stores parts of print line
+            fprintf("----Assigning Aggregate Station ID: %s....", char(parts{1}));
             STATION = INI.mapCompSelected(char(parts{1}));
-            errormsg = '';
-            for emi = 1:sizeparts(2)
-                if emi == 1
-                    errormsg = strcat(errormsg, char(parts{emi}), ' (not calculated) ');
-                else
-                    errormsg = strcat(errormsg, " ", char(parts{emi}));
-                end
-            end
-            fprintf('%s \n',errormsg);
+            % Reches here if new aggregate station name is already in
+            % database
+            fprintf('failed. ID already used.\n');
+            EqReport{1} = strcat(char(parts{1}), ' (not calculated)');
             pass = false;
         catch
             % Aggregate Station name isn't taken
-            EqReport = {}; % Stores parts of print line 
+            fprintf('Success.\n');
             EqRi = 4;
             EqReport{1} = char(parts{1});
             sumi = 2;
+            stationflow = {}; %stores if station is discharge
             stationsexist = {}; % stores if station is found
             stationsdaily = {}; % stores if sation timeseries is daily
             
             % stores scalar for timeseries values
             % Ex + = 1, - = -1, not found = 0
-            stationaddsub = {}; 
+            stationaddsub = {};
             stationi = 1;
             modcheck = false; % used to alternate between parsing of operators and station names
             DateMin = 999999999; % Earliest Date for Aggregated values
@@ -51,7 +75,7 @@ while ischar(tline)
                     else
                         pass = pass & false;
                     end
-                % Third cell should be either station name or a +/-
+                    % Third cell should be either station name or a +/-
                 elseif sumi == 3
                     % If 3rd cell is operator, modcheck changes to match
                     if strcmp('-',char(parts{sumi})) || strcmp('+',char(parts{sumi}))
@@ -60,14 +84,15 @@ while ischar(tline)
                         pass = pass & true;
                     else
                         modcheck = true;
-                        %% If not operator then parse as station
+                        % If not operator then parse as station
                         if mod(sumi,2) == modcheck
                             % Block completes if station found
                             try
+                                fprintf('----Checking Summand Station %s', char(parts{sumi}));
                                 STATION = INI.mapCompSelected(char(parts{sumi}));
                                 tsize = size(STATION.TIMEVECTOR);
                                 daily = true;
-                                % Pulls time values and checks 
+                                % Pulls time values and checks
                                 % if series is daily values
                                 for ti = 2:tsize(1)
                                     if STATION.TIMEVECTOR(ti,1) - STATION.TIMEVECTOR(ti - 1,1) == 1
@@ -80,19 +105,31 @@ while ischar(tline)
                                 stationsdaily{stationi} = daily; % boolean if daily
                                 stationsexist{stationi} = true; % boolean if found
                                 stationsaddsub{stationi} = 1; % scalar for multiplying values
+                                stationsflow{stationi} = strcmpi(STATION.DATATYPE, 'discharge');
+                                if ~stationsflow{stationi}
+                                    EqReport{3} = '';
+                                    fprintf('...Not Flow\n');
+                                elseif ~stationsdaily{stationi}
+                                    EqReport{3} = '';
+                                    fprintf('...Not Daily\n');
+                                end
                                 stations{stationi} = STATION; % stores station for later reference
-                                UsableAggregate = UsableAggregate | (stationsdaily{stationi} & stationsexist{stationi});
-                                DateMin = min(DateMin, STATION.TIMEVECTOR(1,1));
-                                DateMax = max(DateMax, STATION.TIMEVECTOR(end,1));
+                                UsableAggregate = UsableAggregate | (stationsdaily{stationi} & stationsexist{stationi} & stationsflow{stationi});
+                                DateMin = min(DateMin, STATION.TIMEVECTOR(1,1)); % checks if first date in time series is minimum
+                                DateMax = max(DateMax, STATION.TIMEVECTOR(end,1));% checks if last date in time series is maximum
+                                if stationsdaily{stationi} && stationsexist{stationi} && stationsflow{stationi}
+                                    fprintf('...OK\n');
+                                end
                                 stationi = stationi + 1;
                             % station not found
                             catch
-                                
-                                EqReport{3} = strcat(char(parts{3}), ' (not found)');
+                                fprintf('...Not found\n');
+                                EqReport{3} = '';
                                 stationsdaily{stationi} = false; % boolean if daily
                                 stationsexist{stationi} = false; % boolean if found
                                 stationsaddsub{stationi} = 0; % scalar for multiplying values
-                                UsableAggregate = UsableAggregate | (stationsdaily{stationi} & stationsexist{stationi});
+                                stationsflow{stationi} = false;
+                                UsableAggregate = UsableAggregate | false;
                                 stationi = stationi + 1;
                             end
                         end
@@ -101,10 +138,11 @@ while ischar(tline)
                 else
                     if mod(sumi,2) == modcheck
                         try
+                            fprintf('----Checking Summand Station %s', char(parts{sumi}));
                             STATION = INI.mapCompSelected(char(parts{sumi}));
                             tsize = size(STATION.TIMEVECTOR);
                             daily = true;
-                            % Pulls time values and checks 
+                            % Pulls time values and checks
                             % if series is daily values
                             for ti = 2:tsize(1)
                                 if STATION.TIMEVECTOR(ti,1) - STATION.TIMEVECTOR(ti - 1,1) == 1
@@ -116,27 +154,40 @@ while ischar(tline)
                             EqReport{EqRi} = char(parts{sumi});
                             stationsdaily{stationi} = daily; % boolean if daily
                             stationsexist{stationi} = true; % boolean if found
-                            
+                            stationsflow{stationi} = strcmpi(STATION.DATATYPE, 'discharge');
+                            if ~stationsflow{stationi}
+                                EqReport{3} = '';
+                                fprintf('...Not Flow\n');
+                            elseif ~stationsdaily{stationi}
+                                EqReport{3} = '';
+                                fprintf('...Not Daily\n');
+                            end
                             % Parses which scalar to use
                             if strcmp('-',char(parts{sumi - 1}))
                                 stationsaddsub{stationi} = -1;
                             else
                                 stationsaddsub{stationi} = 1;
                             end
-                            UsableAggregate = UsableAggregate | (stationsdaily{stationi} & stationsexist{stationi});
+                            UsableAggregate = UsableAggregate | (stationsdaily{stationi} & stationsexist{stationi} & stationsflow{stationi});
                             stations{stationi} = STATION;
                             
                             % Parse Start and End date to find latest and
                             % earliest dates in all station data
                             DateMin = min(DateMin, STATION.TIMEVECTOR(1,1));
                             DateMax = max(DateMax, STATION.TIMEVECTOR(end,1));
+                            if stationsdaily{stationi} && stationsexist{stationi} && stationsflow{stationi}
+                                fprintf('...OK\n');
+                            end
                             stationi = stationi + 1;
                         catch
-                            EqReport{EqRi} = strcat(char(parts{sumi}), ' (not found)');
+                            fprintf('...Not found\n');
+                            EqReport{sumi - 1} = '';
+                            EqReport{sumi} = '';
                             stationsdaily{stationi} = false; % boolean if daily
                             stationsexist{stationi} = false; % boolean if found
                             stationsaddsub{stationi} = 0; % Scalar multiplier for data
-                            UsableAggregate = UsableAggregate | (stationsdaily{stationi} & stationsexist{stationi});
+                            stationsflow{stationi} = false;
+                            UsableAggregate = UsableAggregate | false;
                             stationi = stationi + 1;
                         end
                     elseif mod(sumi,2) == ~modcheck
@@ -153,15 +204,17 @@ while ischar(tline)
                 sumi = sumi + 1;
             end
             EqStationsSize = size(stationsexist);
-            if UsableAggregate % If daily data was found for at least of station within equation
+            if UsableAggregate & pass % If daily data was found for at least of station within equation
                 TIMEVECTOR(1:1:(DateMax - DateMin + 1), 1) = 0:1:(DateMax - DateMin);
                 TIMEVECTOR = TIMEVECTOR + DateMin;
                 AGGREGATE(1:1:(DateMax - DateMin + 1), 1) = NaN;
                 First(1:1:(DateMax - DateMin + 1), 1) = true;
+                %debugtest = 'Debug Aggregate Test: ';
                 for si = 1:EqStationsSize(2)
                     if(stationsexist{si} && stationsdaily{si})
                         STATION = stations{si};
                         sTS = size(STATION.TIMEVECTOR);
+                        %debugtest = strcat(debugtest, " + ", num2str(stationsaddsub{si}), " * ", num2str(STATION.DCOMPUTED(1)));
                         for tsi = 1:sTS(1)
                             timestep = STATION.TIMEVECTOR(tsi, 1) - DateMin + 1;
                             if First(timestep)
@@ -173,6 +226,7 @@ while ischar(tline)
                         end
                     end
                 end
+                %fprintf('%s = %f\n', debugtest, AGGREGATE(1))
                 AGGSTATION.STATION_NAME = char(parts{1});
                 AGGSTATION.DATATYPE = char('Discharge');
                 AGGSTATION.UNIT = char('feet^3/sec');
@@ -192,21 +246,23 @@ while ischar(tline)
                 AGGSTATION.TIMEVECTOR = TIMEVECTOR;
                 AGGSTATION.DCOMPUTED = AGGREGATE;
                 INI.mapCompSelected(char(parts{1})) = AGGSTATION;
+            elseif ~pass
+                EqReport = {strcat(EqReport{1}, ' (not calculated)')};
             else % If no daily stations found to aggregate data, not calculated
-                EqReport{1} = strcat(EqReport{1}, ' (not calculated)');
+                EqReport = {strcat(EqReport{1}, ' (not calculated)')};
             end
-            reportmsg = '';
-            sR = size(EqReport);
-            % Outputs post aggregate equation summary
-            for emi = 1:sR(2)
-                if emi == 1
-                    reportmsg = strcat(reportmsg, char(EqReport{emi}));
-                else
-                    reportmsg = strcat(reportmsg, " ", char(EqReport{emi}));
-                end
-            end
-            fprintf('%s \n',reportmsg);
         end
+        reportmsg = '';
+        sR = size(EqReport);
+        % Outputs post aggregate equation summary
+        for emi = 1:sR(2)
+            if emi == 1
+                reportmsg = strcat(reportmsg, char(EqReport{emi}));
+            else
+                reportmsg = strcat(reportmsg, " ", char(EqReport{emi}));
+            end
+        end
+        fprintf('----Final Equation: %s \n',reportmsg);
     end
     tline = fgetl(fid);
 end

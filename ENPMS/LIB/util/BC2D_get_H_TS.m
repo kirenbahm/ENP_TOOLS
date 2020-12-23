f29a06f6c35251a9f85896fb40d1ddba0cad800a
kiren <kiren_bahm@nps.gov>
2020-12-23 12:51:24 -0500

    preproc - changed BC2D extrapolation method
    .. and other small changes
    
    BC2D scatteredinterpolant did not specify extrapolation method.
    I added this so it wsa explicit, and changed it from 'linear' to a
    nearest neighbor technique.
    
    This happened in conjunction with the creation of 'fake' boundary
    conditions on the fringes of the model grid where there was no input
    data, and the interpolation would get wacky.

--- ENPMS/LIB/util/BC2D_get_H_TS.m
+++ ENPMS/LIB/util/BC2D_get_H_TS.m
@@ -2,6 +2,7 @@ function [H,X,Y,L] = BC2D_get_H_TS(INI,nTS)
 
 i = 0;
 
+% keys are station names
 allKeys = INI.MAP_H_DATA.keys;
 numKeys = length(allKeys);
 
@@ -10,17 +11,22 @@ Y(1:numKeys) = NaN;
 H(1:numKeys) = NaN;
 L{1,numKeys} = [];
 
+% iterate over each station
 for currentKey = allKeys
     i = i + 1;
+    
+    % A is station data
     A = INI.MAP_H_DATA(char(currentKey));
     %X(i) = A.X_UTM;
     %Y(i) = A.Y_UTM;
     X(i) = A.utmXmeters;
     Y(i) = A.utmYmeters;
+    
+    % if using Fourier technique, 
     if INI.USE_FOURIER_BC2D
-        H(i) = A.dHf(nTS);
+        H(i) = A.dHf(nTS); % dHf is Fourier temporal interpolation
     else
-        H(i) = A.dHd(nTS);
+        H(i) = A.dHd(nTS); % dHd is Julian temporal interpolation
     end
     L{1,i} = currentKey;
 end

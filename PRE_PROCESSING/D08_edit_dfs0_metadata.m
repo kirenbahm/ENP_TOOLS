9eb9b062d1d143f45d290ed5713ba9caa501f35b
kiren <kiren_bahm@nps.gov>
2021-03-02 11:37:34 -0500

    preproc simplification
    simplified preproc scripts and fixed an error where datatype and units
    were hardcoded.
    
    made all three scripts as identical as possible, with the hope that they
    can be combined or duplicat code removed into subfunctions to ensure
    version control.

--- PRE_PROCESSING/D08_edit_dfs0_metadata.m
+++ PRE_PROCESSING/D08_edit_dfs0_metadata.m
@@ -18,17 +18,41 @@ function D08_edit_dfs0_metadata()
 % BEGIN USER INPUT
 % -------------------------------------------------------------------------
 % -------------------------------------------------------------------------
-% -------------------------------------------------------------------------
 % Location of ENPMS library
-% -------------------------------------------------------------------------
-
 INI.MATLAB_SCRIPTS = '../ENPMS/';
-% Add MATLAB_SCRIPTS to path
+
+% -------------------------------------------------------------------------
+% Location of metadata info
+% -------------------------------------------------------------------------
+EXCEL_METADATA_FILE = '../../ENP_FILES/ENP_TOOLS_Sample_Input/Data_Common/dfs0_rename.xlsx';
+
+%DATA_SHEET_NAME = 'Flow_Dataset_Selection';
+DATA_SHEET_NAME = 'Stage_Dataset_Selection';
+
+MIKE_STATION_NAME_COLUMN = 'MIKE_name';
+DFE_STATION_NAME_COLUMN  = 'DFE_station';
+DFE_DATATYPE_NAME_COLUMN = 'DFE_datatype';
+
+% -------------------------------------------------------------------------
+% Input/output directories
+% -------------------------------------------------------------------------
+DIR_DFS0_IN  = '../../ENP_FILES/ENP_TOOLS_Sample_Input/Obs_Processed_MATLAB/in/Stage/';
+DIR_DFS0_OUT = '../../ENP_TOOLS_Output/D08_edit_dfs0_metadata/Stage-renamed/';
+
+% -------------------------------------------------------------------------
+% -------------------------------------------------------------------------
+% END USER INPUT
+% -------------------------------------------------------------------------
+% -------------------------------------------------------------------------
+INI.OBS_FILETYPE = '*.dfs0';
+
+% Import Statements
 try
     addpath(genpath(INI.MATLAB_SCRIPTS));
 catch
     addpath(genpath(INI.MATLAB_SCRIPTS,0));
 end
+
 % -------------------------------------------------------------------------
 % MikeZero Import Statements
 % -------------------------------------------------------------------------
@@ -45,29 +69,6 @@ H = NETaddDfsUtil();
 eval('import DHI.Generic.MikeZero.DFS.*');
 eval('import DHI.Generic.MikeZero.DFS.dfs123.*');
 eval('import DHI.Generic.MikeZero.*');
-
-% -------------------------------------------------------------------------
-% Location of metadata info
-% -------------------------------------------------------------------------
-EXCEL_METADATA_FILE = './dfs0_rename.xlsx';
-
-DATA_SHEET_NAME = 'M06_MODEL_COMP';
-
-MIKE_STATION_NAME_COLUMN = 'Station';
-DFE_STATION_NAME_COLUMN = 'DFE_STATION';
-DFE_DATATYPE_NAME_COLUMN = 'DFE_DATATYPE';
-
-% -------------------------------------------------------------------------
-% Input/output directories
-% -------------------------------------------------------------------------
-DIR_DFS0_IN  = './infiles/';
-DIR_DFS0_OUT = './outfiles/';
-
-% -------------------------------------------------------------------------
-% -------------------------------------------------------------------------
-% END USER INPUT
-% -------------------------------------------------------------------------
-% -------------------------------------------------------------------------
 
 % Create output directories if they don't already exist
 if ~exist(DIR_DFS0_OUT,  'dir'); mkdir(DIR_DFS0_OUT);  end
@@ -103,18 +104,19 @@ if MIKE_STATION_NAME_COLUMN_NUM == -1 || DFE_STATION_NAME_COLUMN_NUM == -1 || DF
 end
 
 %Find Listing of input Dfs0 files
-FILE_FILTER = [DIR_DFS0_IN '*.dfs0']; 
+FILE_FILTER = [DIR_DFS0_IN INI.OBS_FILETYPE]; % list only files with extension *.dfs0
 LISTING  = dir(char(FILE_FILTER)); % list only files input directory with extension *.dfs0
+
 %Loop through files
 n = length(LISTING);
 for i = 1:n
     try
-        % iterate through each item in LISTING
+        % iterate through each item in LISTING struc array (created by 'dir' matlab function)
         s = LISTING(i);
         myFILE = [s.folder '/' s.name]; %%find full file name and path
         NAME = s.name; % get filename
         fprintf('\n... %d/%d:  reading %s ...', i, n, NAME); % report running status
-        [~,myname,myext] = fileparts(myFILE); 
+        [~,myname,myext] = fileparts(myFILE);
         dfs0In = DfsFileFactory.DfsGenericOpen(myFILE); % Open dfs0
         
         % check if dfs0 has multiple items
@@ -151,41 +153,46 @@ for i = 1:n
         
         % Recreate File with updated metadata
         dfs0File  = DfsFileFactory.DfsGenericOpen(myFILE);
+        AppTitle         = dfs0File.FileInfo.ApplicationTitle;
+        AppVersionNo     = dfs0File.FileInfo.ApplicationVersion;
+        NoData           = dfs0File.FileInfo.DeleteValueDouble;
+        ProjWktString    = dfs0File.FileInfo.Projection.WKTString;
+        ProjLong         = dfs0File.FileInfo.Projection.Longitude;
+        ProjLat          = dfs0File.FileInfo.Projection.Latitude;
+        ProjOri          = dfs0File.FileInfo.Projection.Orientation;
+        TimeAxis         = dfs0File.FileInfo.TimeAxis;  
         dfsDoubleOrFloat = dfs0File.ItemInfo.Item(0).DataType;
-        ProjWktString = dfs0File.FileInfo.Projection.WKTString;
-        ProjLong = dfs0File.FileInfo.Projection.Longitude;
-        ProjLat = dfs0File.FileInfo.Projection.Latitude;
-        ProjOri = dfs0File.FileInfo.Projection.Orientation;
-        utmXmeters = dfs0File.ItemInfo.Item(0).ReferenceCoordinateX;
-        utmYmeters = dfs0File.ItemInfo.Item(0).ReferenceCoordinateY;
-        elev_ngvd29_ft = dfs0File.ItemInfo.Item(0).ReferenceCoordinateZ;
+        itemQuantity     = dfs0File.ItemInfo.Item(0).Quantity;
+        utmXmeters       = dfs0File.ItemInfo.Item(0).ReferenceCoordinateX;
+        utmYmeters       = dfs0File.ItemInfo.Item(0).ReferenceCoordinateY;
+        elev_ngvd29_ft   = dfs0File.ItemInfo.Item(0).ReferenceCoordinateZ;
         
         % Read Time Series flow values
         dd = double(Dfs0Util.ReadDfs0DataDouble(dfs0File));
         
         % Read Start datetime
-        yy = double(dfs0File.FileInfo.TimeAxis.StartDateTime.Year);
-        mo = double(dfs0File.FileInfo.TimeAxis.StartDateTime.Month);
-        da = double(dfs0File.FileInfo.TimeAxis.StartDateTime.Day);
-        hh = double(dfs0File.FileInfo.TimeAxis.StartDateTime.Hour);
-        mi = double(dfs0File.FileInfo.TimeAxis.StartDateTime.Minute);
-        se = double(dfs0File.FileInfo.TimeAxis.StartDateTime.Second);
+        yy = double(TimeAxis.StartDateTime.Year);
+        mo = double(TimeAxis.StartDateTime.Month);
+        da = double(TimeAxis.StartDateTime.Day);
+        hh = double(TimeAxis.StartDateTime.Hour);
+        mi = double(TimeAxis.StartDateTime.Minute);
+        se = double(TimeAxis.StartDateTime.Second);
         
         % Create array of time step values 
         START_TIME = datenum(yy,mo,da,hh,mi,se);
         DFS0.T = datenum(dd(:,1));
         DFS0.V = dd(:,2:end);
         
-        %Creat New FileName
+        %Create New FileName
         station_name = char(RAW(rI, MIKE_STATION_NAME_COLUMN_NUM));
         dfs0FileName = [DIR_DFS0_OUT station_name myext];
         factory = DfsFactory();
-        builder = DfsBuilder.Create(station_name,'Matlab DFS',0);
+        builder = DfsBuilder.Create(station_name, char(AppTitle), AppVersionNo);
         
         %save projection and file metadata 
         T = datevec(DFS0.T(1)/86400 + START_TIME);
         builder.SetDataType(0);
-        builder.DeleteValueDouble = -1e-35;
+        builder.DeleteValueDouble = NoData;
         builder.SetGeographicalProjection(factory.CreateProjectionGeoOrigin(ProjWktString,ProjLong,ProjLat,ProjOri));
         builder.SetTemporalAxis(factory.CreateTemporalNonEqCalendarAxis...
             (eumUnit.eumUsec,System.DateTime(T(1),T(2),T(3),T(4),T(5),T(6))));
@@ -194,8 +201,7 @@ for i = 1:n
         item1 = builder.CreateDynamicItemBuilder();
         
         %save item metadata
-        item1.Set(station_name, DHI.Generic.MikeZero.eumQuantity...
-            (eumItem.eumIDischarge,eumUnit.eumUft3PerSec), dfsDoubleOrFloat);
+        item1.Set(station_name, itemQuantity, dfsDoubleOrFloat);
         item1.SetValueType(DataValueType.Instantaneous);
         item1.SetAxis(factory.CreateAxisEqD0());
         item1.SetReferenceCoordinates(utmXmeters,utmYmeters,elev_ngvd29_ft);
@@ -205,19 +211,25 @@ for i = 1:n
             delete(dfs0FileName)
         end
         builder.CreateFile(dfs0FileName);
-        dfs0Out = builder.GetFile();
         
+        dfs = builder.GetFile();
         % Add data in the file
+
         % Write to file using the MatlabDfsUtil
-        MatlabDfsUtil.DfsUtil.WriteDfs0DataDouble(dfs0Out, NET.convertArray(DFS0.T), ...
+        MatlabDfsUtil.DfsUtil.WriteDfs0DataDouble(dfs, NET.convertArray(DFS0.T), ...
             NET.convertArray(DFS0.V, 'System.Double', size(DFS0.V,1), size(DFS0.V,2)))
-        dfs0Out.Close();
- 
+
+        
+        dfs.Close();
+        fprintf(' done');
+        
     catch ME
         fprintf('\n---Error: File Read Failed');
         rethrow(ME)
     end
 end
-fprintf('\n\n Completed.\n\n');
+
+fclose('all');
+fprintf('\n DONE \n\n');
 end
 
